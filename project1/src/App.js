67ba77f9b14c28aa6a361188b018c04a6c03a04a
Jamwalaryan <aryanjamwal29@gmail.com>
2023-04-21 00:34:41 +0530

    aryan

---
+++ project1/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Benefits of using react...</h1>
+        
+          <p>1.components based architecture</p>
+          <p>2.Virtual DOM for efficient updates</p>
+          <p>3.Rich ecosystem and community</p>
+          <p>4.Cross-platform development</p>
+          <p>5.Strong community support</p>
+        
+        <button>Get Started...</button>
+      </header>
+    
+    </div>
+  );
+}
+
+export default App;
